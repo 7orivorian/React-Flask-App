6e4162c6334256cad4f5dc712e3c6c58e463ef16
Tori <7orivorian+github@gmail.com>
2024-11-26 20:51:29 -0500

    Add env var for database url

--- backend/config.py
+++ backend/config.py
@@ -1,11 +1,17 @@
+import os
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mydatabase.db"
+db_url: str = os.getenv("DB_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = db_url
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
